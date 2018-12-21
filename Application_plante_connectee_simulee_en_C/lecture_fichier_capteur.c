6696009d5f45aec64ad0fd9796a32ecfc8df132a
Romain <romain.trauchessec@ynov.com>
2018-12-21 15:29:08 +0100

    final code en C pour lecture fichier

--- Application_plante_connectee_simulee_en_C/lecture_fichier_capteur.c
+++ Application_plante_connectee_simulee_en_C/lecture_fichier_capteur.c
@@ -1,2 +1,20 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    FILE* fichier = NULL;
+
+    fichier = fopen("test_lecture_fichier.txt", "r");
+
+    if (fichier != NULL)
+    {
+
+        while(!feof(fichier))
+        {
+            printf("%c", fgetc(fichier));
+        }
+    }
+
+    return 0;
+}
